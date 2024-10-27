aae8fc59b428593e7a4916c725589539cc6e805c
PHISAN SANTITAMNONT <phisan.chula@gmail.com>
2024-10-27 09:14:31 +0700

    Add files via upload

--- InundtMeterPole.py
+++ InundtMeterPole.py
@@ -32,54 +32,56 @@ class InundatMeter:
         self.DTM = DTM
         self.POLE = 12.0  # meter
         self.PEA_METER = 1.75  # meter from ground
+        self.MSL_METER = DTM + self.PEA_METER
 
-        self.CANVAS_UPPER = 0.05
-        self.CANVAS_LOWER = 0.87
+        self.CANVAS_UPPER = 0.040
+        self.CANVAS_LOWER = 0.875
+        self.NORM_METER = (self.CANVAS_LOWER-self.CANVAS_UPPER)/self.POLE  # pixel per meter
 
         PIC_DIR = Path( r'./pics/' )
         self.Canvas = Image.new("RGBA",  (427,1260), (255,255,255,255) )
+
+        ImageFont.load_default(size=12)
         self.DRAW = ImageDraw.Draw( self.Canvas )
 
         elect_pole = ImageObject( PIC_DIR / 'PEA_Pole.png', self.Canvas )
         elect_pole.Paste( 0.5, 0.5, centroid=True ) 
         #import pdb ; pdb.set_trace()
-        self.green_meter = ImageObject( PIC_DIR / 'WattHourMeter_GREEN.png', self.Canvas )
+        self.green_meter = ImageObject( PIC_DIR/'WattHourMeter_GREEN.png', self.Canvas )
         self.green_meter.Resize( 0.15 )
 
-        self.red_meter = ImageObject( PIC_DIR / 'WattHourMeter_RED.png', self.Canvas )
+        self.red_meter = ImageObject( PIC_DIR/'WattHourMeter_RED.png', self.Canvas )
         self.red_meter.Resize( 0.15 )
 
-        if 0:
+        if 0: # test
             self.green_meter.Paste( 0.5, self.CANVAS_UPPER  )
             self.red_meter.Paste( 0.5, self.CANVAS_LOWER  )
         
     def PlotLevel(self, TEXT, AT_MSL, color ):
-        ImageFont.load_default(size=12)
-        up,lo = self.CANVAS_UPPER, self.CANVAS_LOWER
-        def HEI( AT ):   # normalized!
-            px = (lo-up)/self.POLE  # pixel per meter
+        "draw line and text at specified level"
+        def Calc_row( AT_MSL ):   # normalized!
             #import pdb ; pdb.set_trace()
-            return lo-px*(AT-self.DTM)
-        at_norm = HEI(AT_MSL)
-        at_hei = int( self.Canvas.size[1]*at_norm )
+            return self.CANVAS_LOWER-self.NORM_METER*(AT_MSL-self.DTM)
+        row_norm = Calc_row(AT_MSL)
+        at_hei = int( self.Canvas.size[1]*row_norm )
         self.DRAW.line( [ (10,at_hei), (250,at_hei) ], fill=color, width=5 ) 
         self.DRAW.text( ( 10,at_hei), f'{TEXT}={AT_MSL:+.2f}m.', fill=color, font_size=32 )
-        return at_hei,at_norm 
+        return at_hei,row_norm 
 
     def WetMeter(self, FLOOD_LEVEL ):
-        assert (self.PEA_METER+self.DTM)<=FLOOD_LEVEL
+        assert self.MSL_METER<=FLOOD_LEVEL
         self.PlotLevel( 'DTM', self.DTM, 'brown' ) 
-        at_hei,at_norm = self.PlotLevel( 'PEA', self.PEA_METER+self.DTM, 'red' ) 
+        at_hei,row_norm = self.PlotLevel( 'PEA', self.PEA_METER+self.DTM, 'red' ) 
         #import pdb ; pdb.set_trace()
-        self.red_meter.Paste( 0.55, at_norm )
-        at_hei,at_norm = self.PlotLevel( 'FLOOD', FLOOD_LEVEL, 'blue' ) 
-        self.green_meter.Paste( 0.55, at_norm )
+        self.red_meter.Paste( 0.55, row_norm )
+        at_hei,row_norm = self.PlotLevel( 'FLOOD', FLOOD_LEVEL, 'blue' ) 
+        self.green_meter.Paste( 0.55, row_norm )
 
     def DryMeter(self, FLOOD_LEVEL ):
-        assert (self.PEA_METER+self.DTM)>=FLOOD_LEVEL
+        assert self.MSL_METER>=FLOOD_LEVEL
         self.PlotLevel( 'DTM', self.DTM, 'brown' ) 
-        at_hei,at_norm = self.PlotLevel( 'PEA', self.PEA_METER+self.DTM, 'green' ) 
-        self.green_meter.Paste( 0.55, at_norm )
+        at_hei,row_norm = self.PlotLevel( 'PEA', self.PEA_METER+self.DTM, 'green' ) 
+        self.green_meter.Paste( 0.55, row_norm )
         self.PlotLevel( 'FLOOD', FLOOD_LEVEL, 'blue' ) 
         #import pdb ; pdb.set_trace()
 
@@ -91,13 +93,12 @@ class InundatMeter:
 
 ############################################################################
 if __name__ == '__main__':
-
     im = InundatMeter( DTM=200)
     im.WetMeter( 205.4 )
     print('Writing "DiagramWetMeter.png" ...')
     im.save( "DiagramWetMeter.png")
-
+    ##########################################
     im = InundatMeter( DTM=300)
-    im.DryMeter( FLOOD_LEVEL=201.1 )
+    im.DryMeter( FLOOD_LEVEL=301.1 )
     print('Writing "DiagramDryMeter.png" ...')
     im.save( "DiagramDryMeter.png")
